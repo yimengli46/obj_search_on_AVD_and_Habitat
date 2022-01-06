739eb59145b368101ffe5ec1c8654987186309fc
yimengli46 <peter824360321@gmail.com>
2022-01-05 19:06:10 -0500

    small change

--- compute_semantic_prior.py
+++ compute_semantic_prior.py
@@ -62,7 +62,8 @@ obj_obj_dict = {}
 for k1 in obj_list:
 	obj_obj_dict[k1] = {}
 	for k2 in obj_list:
-		obj_obj_dict[k1][k2] = 0
+		obj_obj_dict[k1][k2] = {}
+		obj_obj_dict[k1][k2]['']
 
 for scene_name in scene_names:
 	npy_file = np.load(f'{semantic_prior_folder}/{scene_name}_prior.npy', allow_pickle=True)
@@ -77,28 +78,10 @@ for scene_name in scene_names:
 			obj_obj_dict[o1][o2] += 1
 			obj_obj_dict[o2][o1] += 1
 
-num_classes = len(obj_list)
-corr_mat_mu = np.zeros((num_classes, num_classes))
-for idx_k1, k1 in enumerate(obj_list):
-	for idx_k2, k2 in enumerate(obj_list):
-		corr_mat_mu[idx_k1][idx_k2] = obj_obj_dict[k1][k2]
 
-max_val = np.max(corr_mat_mu)
-#corr_mat_mu /= max_val
 
-# write correlation matrix image
-ticks = []
-for i in range(len(obj_list)):
-	ticks.append(1*i + 0.5)
-
-plt.figure()
-plt.title(f'ignore room type')
-plt.imshow(corr_mat_mu)
-plt.yticks(ticks, list(obj_obj_dict.keys()))
-plt.xticks(ticks, list(obj_obj_dict.keys()), rotation='vertical')
-plt.colorbar()
-plt.tight_layout()
-plt.show()
-#plt.savefig(f'{semantic_prior_folder}/{room_type}_corr.jpg', dpi=400)
-#plt.close()
-#assert 1==2
+prior_dict = {}
+# compute weight, and distance sigma for each pair
+for idx, k in enumerate(obj_list):
+	obj_dict = {}
+	for k in enumerate(obj_list):
