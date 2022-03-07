fixed SPL computation

analyze_result_npy.py
@@ -9,16 +9,19 @@ for scene_name in scene_list:
 	num_test = len(results_npy.keys())
 
 	suc_list = []
+	SPL = 0
 	for i in range(num_test):
 		result = results_npy[i]
 		flag_suc = result['success']
 		suc_list.append(flag_suc)
+		if flag_suc:
+			SPL += 1. * result['optim_steps'] / result['steps']
 
 	suc_list = np.array(suc_list)
 	suc_rate = np.sum(suc_list) / num_test
 
 	# compute SPL
-	SPL = 0.5 * np.sum(suc_list) / num_test
+	SPL = SPL / num_test
 
 	print(f'scene_name = {scene_name}, suc_rate = {suc_rate}, SPL = {SPL}')
 

localNavigator_Astar.py
@@ -177,12 +177,18 @@ def AStarSearch(start_coords, goal_coords, graph):
 				visited.append(node_loc)
 
 class localNav_Astar:
-	def __init__(self, pose_range, coords_range):
+	def __init__(self, pose_range, coords_range, scene_name):
 		self.pose_range = pose_range
 		self.coords_range = coords_range
 		self.local_map_margin = 30
 		self.path_pose_action = []
 		self.path_idx = -1 # record the index of the agent in the path
+		
+		occ_map_path = f'output/semantic_map/{scene_name}'
+		occupancy_map = np.load(f'{occ_map_path}/BEV_occupancy_map.npy')
+		occupancy_map = np.where(occupancy_map==1, 3, occupancy_map) # free cell
+		occupancy_map = np.where(occupancy_map==0, 1, occupancy_map) # occupied cell
+		self.occupancy_map = occupancy_map
 
 	'''
 	def find_subgoal(self, peak_pose, occupancy_map):
@@ -472,6 +478,197 @@ class localNav_Astar:
 		#zmin = 0
 
 		return (xmin, zmin, xmax, zmax), agent_local_coords, subgoal_local_coords
+
+	def get_gt_number_steps(self, target_pose, agent_pose):
+		agent_coords = pose_to_coords(agent_pose, self.pose_range, self.coords_range)
+		target_coords = pose_to_coords(target_pose, self.pose_range, self.coords_range)
+		#print(f'agent_coords = {agent_coords}')
+
+		#================================ find a reachable subgoal on the map ==============================
+		# get a local map of the occupancy map
+		H, W = self.occupancy_map.shape
+		#print(f'agent_coords = {agent_coords}, peak_coords = {peak_coords}')
+		(xmin, zmin, xmax, zmax), agent_local_coords, target_local_coords = \
+			self._decide_local_map_size(agent_coords, target_coords, H, W)
+		#print(f'xmin = {xmin}, zmin = {zmin}, xmax = {xmax}, zmax = {zmax}')
+		local_occupancy_map = self.occupancy_map[zmin:zmax, xmin:xmax]
+		#local_occupancy_map = occupancy_map.copy()
+
+		'''
+		plt.imshow(local_occupancy_map)
+		plt.show()
+		'''
+		H, W = local_occupancy_map.shape
+		x = np.linspace(0, W-1, W)
+		y = np.linspace(0, H-1, H)
+		xv, yv = np.meshgrid(x, y)
+		map_coords = np.stack((xv, yv), axis=2).astype(np.int16)
+
+		# take the non-obj pixels
+		mask_free = (local_occupancy_map != 1)
+		free_map_coords = map_coords[mask_free]
+
+		if free_map_coords.shape[0] == 0:
+			print(f'no free space cells on the occupancy map')
+			assert 1==2
+
+		#===================== build the graph ======================
+		roadmap = {}
+		num_nodes = free_map_coords.shape[0]
+		for i in range(num_nodes):
+			neighbors = []
+			x, y = free_map_coords[i]
+			# bottom
+			if y+1 < H and mask_free[y+1, x]:
+				neighbors.append((x, y+1))
+			# top
+			if y-1 >= 0 and mask_free[y-1, x]:
+				neighbors.append((x, y-1))
+			# left
+			if x-1 >= 0 and mask_free[y, x-1]:
+				neighbors.append((x-1, y))
+			# right
+			if x+1 < W and mask_free[y, x+1]:
+				neighbors.append((x+1, y))
+			# top left
+			if x-1 >= 0 and y-1 >= 0 and mask_free[y-1, x-1]:
+				neighbors.append((x-1, y-1))
+			# top right
+			if x+1 < W and y-1 >= 0 and mask_free[y-1, x+1]:
+				neighbors.append((x+1, y-1))
+			# bottom left
+			if x-1 >= 0 and y+1 < H and mask_free[y+1, x-1]:
+				neighbors.append((x-1, y+1))
+			# bottom right
+			if x+1 < W and y+1 < H and mask_free[y+1, x+1]:
+				neighbors.append((x+1, y+1))
+
+			roadmap[tuple(free_map_coords[i])] = neighbors
+
+		#print(f'roadmap = {roadmap}')
+		#===================== find the subgoal (closest to peak and reachable from agent)
+		reachable_locs = breadthFirstSearch(agent_local_coords, roadmap)
+		reachable_locs = np.array(list(map(list, reachable_locs)))
+		# return the closest location on the free map
+		manhatten_dist = np.sum(np.absolute(reachable_locs - target_local_coords), axis=1)
+		min_idx = np.argmin(manhatten_dist)
+		subgoal_local_coords = reachable_locs[min_idx]
+		subgoal_coords = (subgoal_local_coords[0]+xmin, subgoal_local_coords[1]+zmin)
+		subgoal_pose = pxl_coords_to_pose(subgoal_coords, self.pose_range, self.coords_range)
+
+		#===================== Using A* to navigate to the subgoal
+		path = AStarSearch(agent_local_coords, subgoal_local_coords, roadmap)
+		#print(f'path = {path}')
+
+		#========================== visualize the path ==========================
+		#'''
+		if False:
+			mask_new = mask_free.astype(np.int16)
+			for loc in path:
+				mask_new[loc[1], loc[0]] = 2
+			mask_new[agent_local_coords[1], agent_local_coords[0]] = 3 # agent cell
+			mask_new[subgoal_local_coords[1], subgoal_local_coords[0]] = 4 # subgoal cell
+			mask_new[peak_local_coords[1], peak_local_coords[0]] = 5 # peak cell
+
+			fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(20, 10))
+			# visualize gt semantic map
+			ax[0].imshow(local_occupancy_map)
+			ax[0].get_xaxis().set_visible(False)
+			ax[0].get_yaxis().set_visible(False)
+			ax[0].set_title('local_occupancy_map')
+			# visualize built semantic map
+			ax[1].imshow(mask_new, vmin=0, vmax=5)
+			ax[1].get_xaxis().set_visible(False)
+			ax[1].get_yaxis().set_visible(False)
+			ax[1].set_title('planned path')
+			#plt.show()
+			fig.savefig(f'{saved_folder}/step_{step}_localPlanner.jpg')
+			plt.close()
+
+		#'''
+
+		#============================== convert path to poses ===================
+		print(f'len(path) = {len(path)}')
+		if len(path) <= 1:
+			print(f'Target Object is too close to agent initial position')
+			assert 1==2
+
+		poses = []
+		actions = []
+		points = []
+		
+		for loc in path:
+			pose = pxl_coords_to_pose((loc[0]+xmin, loc[1]+zmin), self.pose_range, self.coords_range)
+			points.append(pose)
+
+		## compute theta for each point except the last one
+		## theta is in the range [-pi, pi]
+		thetas = []
+		for i in range(len(points) - 1):
+			p1 = points[i]
+			p2 = points[i + 1]
+			current_theta = math.atan2(p2[1]-p1[1], p2[0]-p1[0])
+			thetas.append(current_theta)
+
+		#print(f'len(thetas) = {len(thetas)}, len(points) = {len(points)}')
+		assert len(thetas) == len(points) - 1
+
+		# pose: (x, y, theta)
+		previous_theta = 0
+		for i in range(len(points) - 1):
+			p1 = points[i]
+			p2 = points[i+1]
+
+			current_theta = thetas[i]
+			## so that previous_theta is same as current_theta for the first point
+			if i == 0:
+				previous_theta = map_rot_to_planner_rot(agent_pose[2])
+			#print(f'previous_theta = {math.degrees(previous_theta)}, current_theta = {math.degrees(current_theta)}')
+			## first point is not the result of an action
+			## append an action before introduce a new pose
+			if i != 0:
+				## forward: 0, left: 3, right 2
+				actions.append("MOVE_FORWARD")
+			## after turning, previous theta is changed into current_theta
+			pose = (p1[0], p1[1], previous_theta)
+			poses.append(pose)
+			## first add turning points
+			## decide turn left or turn right, Flase = left, True = Right
+			bool_turn = False
+			minus_cur_pre_theta = minus_theta_fn(previous_theta, current_theta)
+			if minus_cur_pre_theta < 0:
+				bool_turn = True
+			## need to turn more than once, since each turn is 30 degree
+			while abs(minus_theta_fn(previous_theta, current_theta)) > upper_thresh_theta:
+				if bool_turn:
+					previous_theta = minus_theta_fn(upper_thresh_theta, previous_theta)
+					actions.append("TURN_RIGHT")
+				else:
+					previous_theta = plus_theta_fn(upper_thresh_theta, previous_theta)
+					actions.append("TURN_LEFT")
+				pose = (p1[0], p1[1], previous_theta)
+				poses.append(pose)
+			## add one more turning points when change of theta > 15 degree
+			if abs(minus_theta_fn(previous_theta, current_theta)) > lower_thresh_theta:
+				if bool_turn:
+					actions.append("TURN_RIGHT")
+				else:
+					actions.append("TURN_LEFT")
+				pose = (p1[0], p1[1], current_theta)
+				poses.append(pose)
+			## no need to change theta any more
+			previous_theta = current_theta
+			## then add forward points
+
+			## we don't need to add p2 to poses unless p2 is the last point in points
+			if i + 1 == len(points) - 1:
+				actions.append("MOVE_FORWARD")
+				pose = (p2[0], p2[1], current_theta)
+				poses.append(pose)
+		
+		assert len(poses) == (len(actions) + 1)
+
+		return len(actions)
 
 
 

navigator.py
@@ -21,14 +21,14 @@ def nav(env, episode_id, scene_name, scene_height, start_pose, targets, target_c
 	dataset_dir = '/home/yimeng/Datasets/habitat-lab/habitat_nav/build_avd_like_scenes/output/Gibson_Discretized_Dataset'
 	#scene_name = 'Allensville_0'
 	SEED = 10
-	NUM_STEPS = 600
+	NUM_STEPS = 1250
 	cell_size = 0.1
 	flag_vis = False
 	#saved_folder = 'output/TEST_RESULTS'
 	vis_observed_area_from_panorama = False
 	flag_gt_semantic_map = True
 	NUM_STEPS_EXPLORE = 30
-	NUM_STEPS_vis = 30
+	NUM_STEPS_vis = 100
 	detector = 'PanopticSeg'
 	THRESH_REACH = 0.8
 
@@ -42,7 +42,7 @@ def nav(env, episode_id, scene_name, scene_height, start_pose, targets, target_c
 	#occ_map = np.load(f'output/semantic_map/{scene_name}/BEV_occupancy_map.npy', allow_pickle=True)
 
 	PF = ParticleFilter(target_cat, 10000, gt_semantic_map.copy(), pose_range, coords_range)
-	LN = localNav_Astar(pose_range, coords_range)
+	LN = localNav_Astar(pose_range, coords_range, scene_name)
 
 	semMap_module = SemanticMap(scene_name, pose_range, coords_range) # build the observed sem map
 	traverse_lst = []
@@ -76,6 +76,7 @@ def nav(env, episode_id, scene_name, scene_height, start_pose, targets, target_c
 	MODE_FIND_GOAL = False
 	GOAL_list = targets
 	GOAL_POSE_list = [a for (a, b) in GOAL_list]
+	gt_number_steps = -1
 
 	while step < NUM_STEPS:
 		print(f'step = {step}')
@@ -90,6 +91,16 @@ def nav(env, episode_id, scene_name, scene_height, start_pose, targets, target_c
 		pose = (agent_pos[0], agent_pos[2], angle)
 		agent_map_pose = (pose[0], -pose[1], -pose[2])
 		traverse_lst.append(agent_map_pose)
+
+		#=========================== compute optimal number of steps to the goal ========================
+		if step == 0:
+			lst_gt_number_steps = []
+			for (GOAL_Pose, GOAL_size) in GOAL_list:
+				goal_steps = LN.get_gt_number_steps(GOAL_Pose, agent_map_pose)
+				lst_gt_number_steps.append(goal_steps)
+			gt_number_steps = min(lst_gt_number_steps)
+			print(f'closest goal is {gt_number_steps} steps away!')
+
 
 		# add the observed area
 		semMap_module.build_semantic_map(obs, pose, step=step, saved_folder=saved_folder)
@@ -112,7 +123,7 @@ def nav(env, episode_id, scene_name, scene_height, start_pose, targets, target_c
 					if dist_subgoal_to_goal <= 1. + GOAL_size:
 						print(f'==========================REACH THE GOAL =============================')
 						MODE_FIND_GOAL = True
-						return MODE_FIND_GOAL, step
+						return MODE_FIND_GOAL, step, gt_number_steps
 
 		# condition 2: run out of exploration steps
 		elif explore_steps >= NUM_STEPS_EXPLORE:
@@ -240,5 +251,5 @@ def nav(env, episode_id, scene_name, scene_height, start_pose, targets, target_c
 			agent_rot = habitat_sim.utils.common.quat_from_angle_axis(-next_pose[2], habitat_sim.geo.GRAVITY)
 			obs = env.habitat_env.sim.get_observations_at(agent_pos, agent_rot, keep_agent_at_new_pose=True)
 
-	return False, step
+	return False, step, gt_number_steps
 	